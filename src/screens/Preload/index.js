09fedd079b755c59b08e1ea7632982016f4da03f
jrlucas1 <joaolucas.pl151@academico.ifsul.edu.br>
2023-06-15 16:59:22 -0300

    feat: routing das notificações

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,70 +1,88 @@
-import {React, useEffect} from 'react';
-import {View, Text, Alert} from 'react-native';
+import { React, useContext, useEffect } from 'react';
+import { View, Text, Alert } from 'react-native';
 import EncryptedStorage from 'react-native-encrypted-storage'
 import { CommonActions } from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
-import app from '@react-native-firebase/app';
+import { PermissionsAndroid } from 'react-native';
+import { MessagingContext } from '../../context/MessagingProvider'
+import { TabActions } from '@react-navigation/native';
 
-// import { Container } from './styles';
 
-const Preload = ({navigation}) => {
+const Preload = ({ navigation }) => {
 
-  
-async function retrieveUserSession(){
-  try {   
+  const { nav } = useContext(MessagingContext);
+
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS)
+
+  async function retrieveUserSession() {
+    try {
       const session = await EncryptedStorage.getItem('user_session');
-      return session !== null ? JSON.parse(session): null;
-  } catch (error) {
+      return session !== null ? JSON.parse(session) : null;
+    } catch (error) {
       console.error("Preload, retrieveUserSession: " + error);
       return null;
+    }
   }
-}
-const entrar = async () => {
-  const userSession = await retrieveUserSession();
-  console.log(userSession);
-  if (userSession) {
-    try {
-      await auth().signInWithEmailAndPassword(
-        userSession.email,
-        userSession.pass,
-      );
+  const entrar = async () => {
+    const userSession = await retrieveUserSession();
+    console.log(userSession);
+    if (userSession) {
+      try {
+        await auth().signInWithEmailAndPassword(
+          userSession.email,
+          userSession.pass,
+        );
+        switch (nav) {
+          case '1':
+            navigation.navigate('AppStack',
+              { screen: 'Atividades' }
+            );
+            break;
+
+          default:
+            console.log("Default")
+            navigation.dispatch(
+              CommonActions.reset({
+                index: 1,
+                routes: [
+                  { name: 'AppStack' }
+                ]
+              }
+              )
+            )
+            break;
+        }
+      } catch (e) {
+        console.error('SignIn, entrar: ' + e);
+        switch (e.code) {
+          case 'auth/user-not-found':
+            Alert.alert('Erro', 'Usuário não cadastrado.');
+            break;
+          case 'auth/wrong-password':
+            Alert.alert('Erro', 'Erro na senha.');
+            break;
+          case 'auth/invalid-email':
+            Alert.alert('Erro', 'Email inválido.');
+            break;
+          case 'auth/user-disabled':
+            Alert.alert('Erro', 'Usuário desabilitado.');
+            break;
+        }
+      }
+    } else {
       navigation.dispatch(
         CommonActions.reset({
           index: 0,
-          routes: [{name: 'AppStack'}],
+          routes: [{ name: 'SignIn' }],
         }),
       );
-    } catch (e) {
-      console.error('SignIn, entrar: ' + e);
-      switch (e.code) {
-        case 'auth/user-not-found':
-          Alert.alert('Erro', 'Usuário não cadastrado.');
-          break;
-        case 'auth/wrong-password':
-          Alert.alert('Erro', 'Erro na senha.');
-          break;
-        case 'auth/invalid-email':
-          Alert.alert('Erro', 'Email inválido.');
-          break;
-        case 'auth/user-disabled':
-          Alert.alert('Erro', 'Usuário desabilitado.');
-          break;
-      }
     }
-  } else {
-    navigation.dispatch(
-      CommonActions.reset({
-        index: 0,
-        routes: [{name: 'SignIn'}],
-      }),
-    );
-  }
-};
+  };
 
-useEffect(()=>{
-  entrar()
-}
-)
+  useEffect(() => {
+    entrar()
+  }
+  )
   return (
     <View>
       <Text></Text>
